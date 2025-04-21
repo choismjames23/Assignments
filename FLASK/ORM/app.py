dcc3ec6c030b6f54f9508538e8b47db434b5b2cf
choismjames23 <choism23@naver.com>
2025-04-21 23:05:44 +0900

    -비밀번호 수정-

--- FLASK/ORM/app.py
+++ FLASK/ORM/app.py
@@ -4,7 +4,7 @@ from db import db
 from models import User, Board
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:jssd23!!@localhost/oz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:my-password@localhost/oz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
